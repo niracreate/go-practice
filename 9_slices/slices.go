7656b6fc52539c99b50acfe2bf5afb38740db380
Nirakar Bogati <lmntrix.as619@gmail.com>
2025-11-02 21:19:18 +0545

    slices

--- 9_slices/slices.go
+++ 9_slices/slices.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"slices"
+)
 
+func main() {
+	//dynamic array
+	// var nums []int
+	// fmt.Println(nums == nil)
+	// fmt.Println(len(nums))
+
+	// var nums = make([]int, 2, 5)
+	// fmt.Println(nums)
+	// fmt.Println(cap(nums))
+	// nums := []int{}
+	// nums = append(nums, 1)
+	// nums = append(nums, 2)
+	// nums = append(nums, 3)
+	// nums = append(nums, 4)
+
+	// nums[0] = 3
+	// nums[1] = 5
+
+	// nums = append(nums, 5)
+	// fmt.Println(nums)
+	// fmt.Println(cap(nums))
+	// fmt.Println(len(nums))
+
+	// var nums = make([]int, 0, 5)
+	// nums = append(nums, 2)
+	// var nums2 = make([]int, len(nums))
+
+	// //copy function
+
+	// copy(nums2, nums)
+
+	// fmt.Println(nums, nums2)
+
+	//slice operator
+	// var nums = []int{1, 2, 3}
+	// fmt.Println(nums[0:2])
+	// fmt.Println(nums[2:])
+
+	//slice
+
+	var nums1 = []int{1, 2}
+	var nums2 = []int{1, 2}
+	fmt.Println(slices.Equal(nums1, nums2))
 }
