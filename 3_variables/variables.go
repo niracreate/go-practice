afd80ae0ac8e87f47c74d27bb6ea1a28cec65b27
Nirakar Bogati <lmntrix.as619@gmail.com>
2025-11-13 20:11:12 +0545

    basics

--- 3_variables/variables.go
+++ 3_variables/variables.go
@@ -2,8 +2,6 @@ package main
 
 import "fmt"
 
-
-
 func main() {
 	// var name string = "golang"
 	var name = "golang"
--- 4_constants/constants.go
+++ 4_constants/constants.go
@@ -3,15 +3,14 @@ package main
 import "fmt"
 
 // const age = 20
-
 func main() {
 	// const name = "golang"
 	// fmt.Println(age)
-
-const(
-	port = 5000
-	host = "localhost"
-)
-fmt.Println(port,host)
+ 	//constant grouping 
+	const (
+		port = 5000
+		host = "localhost"
+	)
+	fmt.Println(port, host)
 
 }
