c054039b536316714424f85a233d8d80fac6246a
Nirakar Bogati <lmntrix.as619@gmail.com>
2025-11-20 12:44:59 +0545

    interface

---
+++ 17_interfaces/interface.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type payment struct{}
+
+func (p payment) makePayment(amount float32) {
+	// razorpayPaymentGw := razorpay{}
+	stripePaymentGw := stripe{}
+	stripePaymentGw.pay(amount)
+
+	// razorpayPaymentGw.pay(amount)
+}
+
+// type razorpay struct {
+// }
+
+// func (r razorpay) pay(amount float32) {
+
+// 	//logic to make payment
+// 	fmt.Println("making payment using razor pay", amount)
+// }
+
+type stripe struct{}
+
+func (s stripe) pay(amount float32) {
+	fmt.Println("making payment using stripe", amount)
+}
+
+func main() {
+	newPayment := payment{}
+	newPayment.makePayment(100)
+	// stripePayment := stripe{}
+	// stripePayment.pay(200)
+
+}
