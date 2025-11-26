ca3c7c81082d93f63d429c67d01bc7b4500903a4
Nirakar Bogati <lmntrix.as619@gmail.com>
2025-11-26 08:04:58 +0545

    enums

--- 18_enums/enums.go
+++ 18_enums/enums.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
+
+type OrderStatus int
+
+const (
+	Received OrderStatus = iota
+	Confirmed 
+	Prepared 
+	Delivered
+)
+
+
+func changeOrderStatus(status string) {
+	fmt.Println("chaning order status to" , status)
+}
 func main() {
-	
+	changeOrderStatus("confirmed")
 }
