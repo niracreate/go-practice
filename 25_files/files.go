c9a8c3950091dcde1006ba43cd2a59b246d1748b
Nirakar Bogati <lmntrix.as619@gmail.com>
2025-12-02 13:05:39 +0545

    files

--- 25_files/files.go
+++ 25_files/files.go
@@ -1,19 +1,7 @@
-// write a go code that creates a file inside current dirrectory names hello.txt
-package main
+ package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	// Create a file named "hello.txt" in the current directory
-	file, err := os.Create("hello.txt")
-	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
-	}
-	defer file.Close()
-
-	fmt.Println("File 'hello.txt' created successfully")
+	fmt.Println("test")
 }
